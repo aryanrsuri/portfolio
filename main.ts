2b5bfc955b1cd8426a78d76de783eb819a257111
Aryan Suri <arysuri@proton.me>
2024-05-13 19:11:48 -0700

    13 5 2024 dailies

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ const index = await Deno.readFile("index.html");
 const math = await Deno.readFile("math.html");
 const dangers = await Deno.readFile("./articles/dangers.html")
 const bolognese = await Deno.readFile("./articles/bolognese.xhtml")
+const dalies13052024= await Deno.readFile("./articles/dailies13052024.xhtml")
 const app = new Hono();
 app
   .get("/", (c) => c.text(index))
   .get("/index.html", (c) => c.text(index))
   .get("/math.html", (c) => c.text(math))
   .get("/dangers.html", (c) => c.text(dangers))
+  .get("/dailies13052024.html", (c) => c.text(dalies13052024))
   .get("/recipes/bolognese.html", (c) => c.text(bolognese))
   .get(
     "/:any",
