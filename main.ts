59a7c53563f73d640964ae668c48ee3ed6831222
Aryan Suri <arysuri@proton.me>
2024-05-29 21:32:36 -0700

    refactor w serveStatic
    Signed-off-by: Aryan Suri <arysuri@proton.me>

--- main.ts
+++ main.ts
@@ -1,20 +1,11 @@
-import { Hono } from "https://deno.land/x/hono@v3.12.3/mod.ts";
-const index = await Deno.readFile("index.html");
-const math = await Deno.readFile("math.html");
-const dangers = await Deno.readFile("./articles/dangers.html")
-const bolognese = await Deno.readFile("./articles/bolognese.xhtml")
-const dalies13052024= await Deno.readFile("./articles/dailies13052024.xhtml")
+import { Hono, HonoRequest } from "https://deno.land/x/hono@v4.3.11/mod.ts";
+import { serveStatic } from "https://deno.land/x/hono@v4.3.11/middleware.ts";
 const app = new Hono();
 app
-  .get("/", (c) => c.text(index))
-  .get("/index.html", (c) => c.text(index))
-  .get("/math.html", (c) => c.text(math))
-  .get("/dangers.html", (c) => c.text(dangers))
-  .get("/dailies13052024.html", (c) => c.text(dalies13052024))
-  .get("/recipes/bolognese.html", (c) => c.text(bolognese))
+  .use("/*", serveStatic({ root: "./src" }))
   .get(
     "/:any",
-    (c) =>
+    (c: HonoRequest) =>
       c.html(
         `<p style="font-family: 'Lucida Console', monospace;">go back to <a href="/">/</a> please</p>`,
       ),
